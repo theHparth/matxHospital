a900314fd80d637720ef64033415a82f2d8147e2
theHparth <pphirpara@gmail.com>
2022-04-18 19:08:26 -0400

    all delete

--- routes/stockQty.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { getTotoalQtyofStock } from "../controllers/totalQtyController.js";
-
-router.route("/").get(getTotoalQtyofStock);
-
-export default router;
